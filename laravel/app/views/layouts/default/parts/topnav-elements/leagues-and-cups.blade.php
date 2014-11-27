05148f36085fc73d76ab6394bb5236caf7ad1a3b
Martin Evans <martin.evans@phpcoder.guru>
2014-11-27 21:44:31 +0000

    Top Navigation Elements

---
+++ laravel/app/views/layouts/default/parts/topnav-elements/leagues-and-cups.blade.php
@@ -0,0 +1,10 @@
+<li class="dropdown leagues-and-clubs-dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Leagues &amp; Cups <b class="caret"> </b></a>
+    <ul class="dropdown-menu">
+        <li><a href="">World Cup</a></li>
+        <li><a href="">International</a></li>
+        <li><a href="">Worldwide Club</a></li>
+        <li><a href="">Europe</a></li>
+        <li><a href="">England</a></li>
+    </ul>
+</li>
---
+++ laravel/app/views/layouts/default/parts/topnav-elements/nations-and-clubs.blade.php
@@ -0,0 +1,11 @@
+<li class="dropdown nations-and-cups-dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nations &amp; Clubs <b class="caret"> </b></a>
+    <ul class="dropdown-menu">
+        <li><a href="">Nations</a></li>
+        <li><a href="">Under-21s</a></li>
+        <li><a href="">Major Clubs</a></li>
+        <li><a href="">League</a></li>
+        <li><a href="">Non-League</a></li>
+        <li><a href="">Other</a></li>
+    </ul>
+</li>
