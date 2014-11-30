afcf1f601e4f0ebcf6415aa5190ba2a8ef918e44
Martin Evans <martin.evans@phpcoder.guru>
2014-11-30 21:19:21 +0000

    bundesliga configurations

---
+++ laravel/app/config/game-pre-requisites/revision/current/leagues/bundesliga/dortmund.php
@@ -0,0 +1,187 @@
+<?php
+
+return [
+    'players' => [
+        'hendrik_bonmann' => [
+            'position'          => ['gk'],
+            'full_detail_only'  => true,
+            'squad_number'      => 39,
+            'nationality'       => 'germany'
+        ],
+        'zlatan_alomerovic' => [
+            'position'          => ['gk'],
+            'full_detail_only'  => true,
+            'squad_number'      => 33,
+            'nationality'       => 'germany'
+        ],
+        'mitchell_langerak' => [
+            'position'          => ['gk'],
+            'squad_number'      => 22,
+            'nationality'       => 'australia'
+        ],
+        'roman_weidenfeller' => [
+            'position'          => ['gk'],
+            'squad_number'      => 1,
+            'nationality'       => 'germany'
+        ],
+        'neven_subotic' => [
+            'position'          => ['cb'],
+            'squad_number'      => 4,
+            'nationality'       => 'serbia'
+        ],
+        'mats_julian_hummels' => [
+            'position'          => ['cb'],
+            'squad_number'      => 15,
+            'nationality'       => 'germany'
+        ],
+        'marian_sarr' => [
+            'position'          => ['cb'],
+            'full_detail_only'  => true,
+            'squad_number'      => 24,
+            'nationality'       => 'germany'
+        ],
+        'sokratis_papastathopolous' => [
+            'position'          => ['cb'],
+            'display_name'      => 'sokratis',
+            'squad_number'      => 25,
+            'nationality'       => 'greece'
+        ],
+        'lukasz_piszczek' => [
+            'position'          => ['rb','rwb'],
+            'squad_number'      => 26,
+            'nationality'       => 'poland'
+        ],
+        'marcel_schmelzer' => [
+            'position'          => ['lb'],
+            'squad_number'      => 29,
+            'nationality'       => 'germany'
+        ],
+        'jannik_bandowski' => [
+            'position'          => ['cb'],
+            'full_detail_only'  => true,
+            'squad_number'      => 35,
+            'nationality'       => 'germany'
+        ],
+        'jon_gorenc-stankovic' => [
+            'position'          => ['cb'],
+            'full_detail_only'  => true,
+            'squad_number'      => 47,
+            'nationality'       => 'slovenia'
+        ],
+        'mattias_ginter' => [
+            'position'          => ['cb','lb','rb'],
+            'squad_number'      => 28,
+            'nationality'       => 'germany'
+        ],
+        'erik_durm' => [
+            'position'          => ['lb'],
+            'squad_number'      => 37,
+            'nationality'       => 'germany'
+        ],
+        'khald_narey' => [
+            'position'          => ['cb'],
+            'full_detail_only'  => true,
+            'squad_number'      => 30,
+            'nationality'       => 'germany'
+        ],
+        'march_hornschuh' => [
+            'position'          => ['cb'],
+            'full_detail_only'  => true,
+            'squad_number'      => 3,
+            'nationality'       => 'germany'
+        ],
+        'joseph-claude_gyau' => [
+            'position'          => ['cm'],
+            'squad_number'      => 38,
+            'nationality'       => 'united_states_of_america'
+        ],
+        'oliver_kirch' => [
+            'position'          => ['cm'],
+            'full_detail_only'  => true,
+            'squad_number'      => 21,
+            'nationality'       => 'germany'
+        ],
+        'sebastian_kehl' => [
+            'position'          => ['cm'],
+            'squad_number'      => 5,
+            'nationality'       => 'germany'
+        ],
+        'sven_bender' => [
+            'position'          => ['cm'],
+            'squad_number'      => 6,
+            'nationality'       => 'germany'
+        ],
+        'ilkay_gündogan' => [
+            'position'          => ['cm'],
+            'squad_number'      => 8,
+            'nationality'       => 'germany'
+        ],
+        'henrikh_mkhitaryan' => [
+            'position'          => ['cm'],
+            'squad_number'      => 10,
+            'nationality'       => 'armenia'
+        ],
+        'marco_reus' => [
+            'position'          => ['lm','lf','st'],
+            'squad_number'      => 11,
+            'nationality'       => 'germany'
+        ],
+        'jakub_blaszczykowski' => [
+            'position'          => ['rm','rf','rw'],
+            'squad_number'      => 16,
+            'nationality'       => 'poland'
+        ],
+        'nuri_sahin' => [
+            'position'          => ['cm'],
+            'squad_number'      => 18,
+            'nationality'       => 'turkey'
+        ],
+        'kevin_großkreutz' => [
+            'position'          => ['lm'],
+            'squad_number'      => 19,
+            'nationality'       => 'germany'
+        ],
+        'pierre-emerick_aubameyang' => [
+            'position'          => ['cam','st'],
+            'squad_number'      => 17,
+            'nationality'       => 'gabon'
+        ],
+        'mustafa_mohammad_amini' => [
+            'position'          => ['cm'],
+            'squad_number'      => 41,
+            'full_detail_only'  => true,
+            'nationality'       => 'australia'
+        ],
+        'milos_jojic' => [
+            'position'          => ['cam'],
+            'squad_number'      => 14,
+            'full_detail_only'  => true,
+            'nationality'       => 'serbia'
+        ],
+        'mitsuru_maruoka' => [
+            'position'          => ['cm'],
+            'squad_number'      => 31,
+            'nationality'       => 'japan'
+        ],
+        'shinji_kagawa' => [
+            'position'          => ['cam'],
+            'squad_number'      => 7,
+            'nationality'       => 'japan'
+        ],
+        'adrián_ramos' => [
+            'position'          => ['st','cf'],
+            'squad_number'      => 20,
+            'nationality'       => 'columbia'
+        ],
+        'dong-won_ji' => [
+            'position'          => ['st'],
+            'squad_number'      => 23,
+            'nationality'       => 'south_korea'
+        ],
+        'ciro_immobile' => [
+            'position'          => ['st'],
+            'squad_number'      => 9,
+            'nationality'       => 'italy'
+        ]
+    ]
+];
