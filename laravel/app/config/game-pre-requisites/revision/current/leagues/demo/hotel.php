f874cca3cb777db13aa856003fc46926a1fc3c9e
Martin Evans <martin.evans@phpcoder.guru>
2014-12-02 22:34:49 +0000

    cloned dummy data for using in demo league

---
+++ laravel/app/config/game-pre-requisites/revision/current/leagues/demo/hotel.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+    'players' => [
+        'andy_beevers' => [
+            'position'      => ['gk'],
+            'nationality'   => 'england',
+            'squad_number'  => 1
+        ],
+        'charles_darwini' => [
+            'position'      => ['lb'],
+            'nationality'   => 'united states of america',
+            'squad_number'  => 3
+        ],
+        'frank_de_gooy' => [
+            'position'      => ['cb'],
+            'nationality'   => 'holland',
+            'squad_number'  => 5
+        ],
+        'hank_izaguirre' => [
+            'position'      => ['cb'],
+            'nationality'   => 'spain',
+            'squad_number'  => 6
+        ],
+        'jonathan_kellett' => [
+            'position'      => ['rb'],
+            'nationality'   => 'england',
+            'squad_number'  => 2
+        ],
+        'leeroy_minkins' => [
+            'position'      => ['lm'],
+            'nationality'   => 'england',
+            'squad_number'  => 11
+        ],
+        'manuel_nora' => [
+            'position'      => ['rm'],
+            'nationality'   => 'portugal',
+            'squad_number'  => 7
+        ],
+        'oriol_papadopolous' => [
+            'position'      => ['cm'],
+            'nationality'   => 'greece',
+            'squad_number'  => 4
+        ],
+        'quentin_rogers'    => [
+            'position'      => ['cm'],
+            'nationality'   => 'wales',
+            'squad_number'  => 8
+        ],
+        'saul_ti' => [
+            'position' => ['st'],
+            'nationality' => 'japan',
+            'squad_number' => 10
+        ],
+        'ulysses_vega' => [
+            'position' => ['st'],
+            'nationality' => 'columbia',
+            'squad_number' => 9
+        ],
+        'weylon_xero' => [
+            'position' => ['st'],
+            'nationality' => 'peru',
+            'squad_number' => 17
+        ],
+        'yu_ze' => [
+            'position' => ['rb'],
+            'nationality' => 'china',
+            'squad_number' => 14
+        ]
+    ]
+];
